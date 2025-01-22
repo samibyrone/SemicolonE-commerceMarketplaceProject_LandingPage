ef2b3b5e132eb5500605eca5bcd171a55222c0f6
samibyrone <samsonibironke2@gmail.com>
2025-01-22 09:43:05 +0100

    Ecommerce page Updated

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import { IoHeartOutline, IoCartOutline } from "react-icons/io5";
-import { Badges } from '../CustomComponent';
+import { Badges, Title } from '../CustomComponent';
+import { useDispatch, useSelector } from "react-redux";
+import { CartActions, selectedTotalPrice, selectedTotalQuantity } from '../../redux/slice/CartSlice';
 
 export const Cart = () => {
+
+    const totalQuantity = useSelector(selectedTotalQuantity);
+    const cartitems = useSelector((state) => state.cart.itemList);
+    const totalPrice = useSelector(selectedTotalPrice);
 
     const [isOpen, setIsopen] = useState(false);
     const [isClosing, setIsClosing] = useState(false);
@@ -38,7 +44,7 @@ export const Cart = () => {
             <button className="relative z-20" onClick={openCart}> 
                 <IoCartOutline size={25} />                    
                 <div className="absolute -top-2 -right-1.5">    
-                    <Badges color="bg-primary-green">0</Badges>
+                    <Badges color="bg-primary-green">{totalQuantity}</Badges>
                 </div>
             </button>
             
@@ -58,7 +64,7 @@ export const Cart = () => {
                             >
                                 Shopping Cart
                                 <span className='w-7 h-7 text-[11px] font-normal rounded-full text-white grid place-content-center bg-pr'>
-                                    0
+                                    {totalQuantity}
                                 </span>
                             </button>
                             <button 
@@ -73,12 +79,80 @@ export const Cart = () => {
                             </button>
                         </div>
                         <div className="line-container">
-                            <div className={`line ${activeTab === "cart" ? "active" : ""}`}
-                            ></div>
+                            <div 
+                                className={`line ${activeTab === "cart" ? "active" : ""}`}
+                            >
+                            </div>
+                            <div 
+                                className={`line ${activeTab === "wishlist" ? "active" : ""}`}
+                            >
+                            </div>
                         </div>
+                        {activeTab == "cart" ? (
+                            <>
+                                {cartitems.map((item) => (
+                                    <CartProduct
+                                        key={item.id}
+                                        id={item.id}
+                                        cover={item.cover}
+                                        name={item.name}
+                                        price={item.price}
+                                        quantity={item.quantity}
+                                    />
+                                ))}
+                            
+                                <div className='total flex items-center justify-between mt-10'>
+                                    <Title level={6}>SubTotal: </Title>
+                                    <Title level={6}>${totalPrice.toFixed(2)}</Title>    
+                                </div>
+                                <div className='checkout'>
+                                    <button className='primary-btn w-full'>View Cart</button>    
+                                </div>
+                            </>
+                            ) : (
+                             <>show product</>
+                        )}
                     </div>
                 </>
             )}
         </>
     );
 };
+
+
+export const CartProduct = ({id, cover, name, quantity}) => {
+
+    const dispatch = useDispatch();
+
+    const removeCartItems = () => {
+        dispatch(CartActions.removeAllCartItem(id));
+    };
+
+    return (
+        <>
+            <div className='mt-5 border-b-2 border-gray-200 pb-5'>
+                <div className='flex items-center gap-5'>
+                    <div className='images w-20 h-20'>
+                        {cover?.slice(0, 1).map((images, index) => (
+                            <img 
+                                src={images?.image} 
+                                alt="" 
+                                key={index}
+                                className='w-full h-full object-cover'
+                            />
+                        ))}
+                    </div>
+                    <div className='details w-1/2'>
+                        <BodyOne>{name}</BodyOne>
+                        <p className='text-primary-green'>
+                            {quantity} * ${price?.toFixed(2)}
+                        </p>
+                    </div>
+                    <button className='w-10 h-10 bg-gray-200 flex items-center justify-center rounded-full text-primary'>
+                        <IoCloseOutLine size={25} onClick={removeCartItems}/>
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/redux/slice/CartSlice.js
+++ src/redux/slice/CartSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSelector, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {};
 
@@ -12,7 +12,9 @@ const cartSlice = createSlice({
         addToCart(state, action) {
             const newItem = action.payload;
 
-            const existingItem = state.itemList.find((item) => item.id === newItem);
+            const existingItem = state.itemList.find(
+                (item) => item.id === newItem.id
+            );
 
             if (existingItem) {
                 existingItem.quantity++;
@@ -29,11 +31,44 @@ const cartSlice = createSlice({
                 state.totalQuantity++;
             }
         },
+
+        removeCartItem(state, action) {
+            const id = action.payload;
+
+            const existingItem = state.itemList.find((item) => item.id === id);
+            if(existingItem.quantity === 1) {
+                state.itemList = state.itemList.filter((item) => item.id !== id);
+                state.totalQuantity--;
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice -= existingItem.price;
+            }
+        },
+
+        removeAllCartItem(state, action) {
+            const id = action.payload;
+
+            state.itemList = state.itemList.filter((item) => item.id !== id);
+            state.totalQuantity -= state.itemList.reduce(
+                (accountItem, item) => accountItem + item.quantity,
+                0
+            );
+        },
     },
 });
 
 
 export const CartActions = cartSlice.actions;
 export const {} = cartSlice.actions;
+ 
+export const selectedTotalQuantity = createSelector(
+    (state) => state.cart.itemList,
+    (itemList) => itemList.reduce((account) => account + 1, 0)
+);
 
-export default cartSlice.reducer;
+export const selectedTotalPrice = createSelector(
+    (state) => state.cart.itemList,
+    (itemList) => itemList.reduce((account, item) => account + item.totalPrice, 0)
+);
+
+export default cartSlice;
